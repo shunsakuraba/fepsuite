74a1cea95792afc096f448d883099b47e1d9738a
Shun Sakuraba <sakuraba.shun@qst.go.jp>
2020-08-03 16:55:09 +0900

    Draft for tsubame3

---
+++ example_submit_script/titech-tsubame3.zsh
@@ -0,0 +1,95 @@
+#!/bin/zsh
+#$ -cwd
+#$ -g _GROUP_ID_
+
+export ABFE_ROOT=_ABFE_PATH_
+source para_conf.zsh
+
+if [[ -z $JOB_NAME ]]; then
+  # submit itself
+  CUR=${PWD##*/}
+  ID=$1
+  if [[ -n $ID ]]; then
+    shift
+  fi
+  jobs=($@)
+  if [[ -z $ID ]] || [[ -z $jobs ]]; then
+    echo "Usage: $0 (runpath) (stages)"
+    exit 1
+  fi
+  for i in $jobs; do
+    PROCS=1    # Default procs
+    TPP=1      # Threads / proc
+    CPP=1      # Cores / proc
+    CPN=4      # Cores / node. Real procs are 28 but we want one GPU per run
+    SYSTEMCPN=4
+    T=24       # 8 hr run. Modify this part depending on your system size
+    DEPENDS=()
+    EXTRA=()
+    waitcmd=()
+    eval $($ABFE_ROOT/pipeline.zsh query $i)
+    # edit this qstat analysis part to adapt to other job systems
+    qs=$(qstat)
+    for d in $DEPENDS; do
+        if [[ -e $ID.jobid ]] && grep -q "$CUR.$d\s$ID" $ID.jobid ; then
+            if grep -q $res <<< $qs; then
+                waitcmd+=(-hold_jid $ID.$d)
+            fi
+        fi
+    done
+    (( NODES = (PROCS + (CPN/CPP) - 1) / (CPN/CPP) ))
+    (( REQPROCS = NODES * (SYSTEMCPN / CPP) ))
+    case $PROCS in
+        1)
+            RESOURCE=q_node
+            ;;
+        2)
+            RESOURCE=h_node
+            ;;
+        *)
+            RESOURCE=f_node
+            ;;
+    esac
+    NNODE=$NODES
+    set -e 
+    # edit this line to adapt to other job systems
+    cmd=(qsub $waitcmd $EXTRA -v "NAME=$CUR.$i,PROCS=$PROCS,CPN=$CPN,ID=$ID" -l $RESOURCE=$NNODE -l h_rt=$T -N "$ID.$i" $0)
+    builtin echo $cmd
+    # edit this line to adapt to other job systems
+    res=$($cmd | head -1)
+    echo Submitted jobid $res.
+    echo "$CUR.$i\t$ID\t$res" >> $ID.jobid
+    set +e
+  done
+  exit $?
+fi
+
+STEPNO=${NAME:e}
+echo "STEP=$STEPNO"
+
+# Site-specific settings starts from here
+source /etc/profile.d/modules.sh
+
+module load cuda/9.2.148 intel-mpi/19.7.217
+module load gromacs/2019.4
+
+mpirun_() {
+    np=$1
+    shift
+    # edit this part based on environments
+    mpirun -np $np -ppn $CPN $@
+}
+# some supercomputer (e.g. Cray KNL) requires extra commands before single-process run. E.g. (aprun -n 1)
+SINGLERUN=()
+# ---- Site-specific settings ends here
+
+PYTHON3=python3
+MOL=${ID##*-}
+
+GMX=$(which gmx_mpi)
+GMX_MPI=$(which gmx_mpi)
+
+# actual runs
+
+source $ABFE_ROOT/pipeline.zsh run $STEPNO
+
